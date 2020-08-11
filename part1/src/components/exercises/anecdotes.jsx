c4b109ec1fcf4790413347c05a5f0551dd58d106
coolfe <coolfe36@gmail.com>
2020-08-11 10:48:36 +0800

    feat: add exercises 1.14

--- part1/src/components/exercises/anecdotes.jsx
+++ part1/src/components/exercises/anecdotes.jsx
@@ -15,21 +15,44 @@ const Anecdotes = () => {
   };
 
   const [voted, setVoted] = useState({});
+  const [info, setInfo] = useState("");
   const voteAction = () => {
     voted[selected] =
       typeof voted[selected] === "undefined" ? 1 : voted[selected] + 1;
     setVoted({ ...voted });
-    console.log(voted);
+    // console.log(voted);
+    getMaxInfo({ ...voted });
+  };
+
+  const getMaxInfo = (obj) => {
+    if (obj) {
+      const data = Object.values(obj);
+      const maxValue = Math.max(...data);
+      let maxIndex = null;
+      const objKey = Object.keys(obj);
+      for (let i = 0; i < objKey.length; i++) {
+        if (obj[objKey[i]] == maxValue) {
+          maxIndex = i;
+          break;
+        }
+      }
+      setInfo(anecdotes[maxIndex]);
+    } else {
+      setInfo(anecdotes[0]);
+    }
   };
 
   return (
     <div>
       <p>
-        {selected}: {anecdotes[selected]}
+        {selected + 1}: {anecdotes[selected]}
       </p>
       <p>has {voted[selected] || 0} votes</p>
       <button onClick={setSelectedAction}>next</button>
       <button onClick={voteAction}>vote</button>
+      <p>most votes</p>
+      <div>{info}</div>
+      <p></p>
     </div>
   );
 };
